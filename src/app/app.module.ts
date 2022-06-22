ea09401ef713bad7e9d81174c93434a722f17303
dioufcodeur <modoudiouf535@gmail.com>
2022-06-22 11:42:18 +0000

    tec-project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { BoutiqueComponent } from './boutique/boutique.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MapComponent } from './share/map/map.component';
+import { ReferencesComponent } from './share/references/references.component';
+import { SlideHomepageComponent } from './share/slide-homepage/slide-homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    BoutiqueComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomepageComponent,
+    MapComponent,
+    ReferencesComponent,
+    SlideHomepageComponent
   ],
   imports: [
     BrowserModule

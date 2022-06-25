1c462c6d8e9add60332a3d8fc3ae7d61dc73fef1
dioufcodeur <modoudiouf535@gmail.com>
2022-06-25 19:56:12 +0000

    first push from to improve

--- src/js/main.js
+++ src/js/main.js
@@ -50,6 +50,33 @@ $(document).ready(function(){
         ]
     });
 
+    $(".tabs_section, .tabs_section1").slick({
+        slidesToShow: 4,
+        // slidesToScroll: 1,
+        dots: false,
+        arrows: false,
+        autoplay: false,
+        // nextArrow: '<i class="fa fa-arrow-left fa-2x mean_background text-white p-1 px-2 rounded-pill positionned_arrow"></i>',
+        // prevArrow: '<i class="fa fa-arrow-right fa-2x mean_background text-white p-1 px-2 rounded-pill positionned_arrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    centerMode: false 
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true,
+                }
+            }
+        ]
+    });
+
     $('.actu-news_slider').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
